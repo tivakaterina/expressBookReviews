0a9f7e25e778fd7e2fba5544ba60211257aa19a3
Katsiaryna Vasileuskaya <tivakaterina@gmail.com>
2026-01-04 14:39:07 -0500

    step 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,18 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book) {
+      res.send(JSON.stringify(book));
+    } else {
+      res.status(404).send("Book not found");
+    }
  });
   
 // Get book details based on author
