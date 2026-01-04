c1fdf0d08165994a65cb3e86da28834d0a28bf26
Katsiaryna Vasileuskaya <tivakaterina@gmail.com>
2026-01-04 16:37:29 -0500

    register + get endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,13 +6,24 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({message: "Username and password are required"})
+  }
+
+  if (users.find(user => user.username === username )) {
+    return res.status(400).json({message: "Username already exists"})
+  }
+
+  users.push({username: username, password: password})
+  return res.status(200).json({message: "User successfully registered. Now you can login"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books));
+  return res.status(200).type(json).send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
@@ -20,9 +31,9 @@ public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     const book = books[isbn];
     if (book) {
-      res.send(JSON.stringify(book));
+      res.status(200).json(book);
     } else {
-      res.status(404).send("Book not found");
+      res.status(404).json({ message: "Book not found" });
     }
  });
   
